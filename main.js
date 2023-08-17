101ffea7d20854425955ec8d74189d85f614ecd6
Akash Singh <akashkumarsingh11032001@gmail.com>
2023-08-17 15:49:55 +0530

    ./

--- main.js
+++ main.js
@@ -10,7 +10,23 @@ song.onloadedmetadata = function () {
 function playPause() {
   if (ctrlIcon.classList.contains("fa-pause")) {
     song.pause();
+    ctrlIcon.classList.remove("fa-pause");
+    ctrlIcon.classList.add("fa-play");
+
   } else {
     song.play();
+    ctrlIcon.classList.remove("fa-play");
+    ctrlIcon.classList.add("fa-pause");
   }
+}
+
+if(song.play()){
+    setInterval(()=>{
+        progress.value = song.currentTime;
+    },500);
+}
+
+progress.onchange = function(){
+    song.play();
+    song.currentTime = progress.value;
 }
